eeff7b241b981c1434a9292081c0db24b8e549e5
ArHei <47974374+ArHei@users.noreply.github.com>
2021-12-28 18:18:34 +0800

    Add files via upload

---
+++ Untitled1_rename.ps1
@@ -0,0 +1,22 @@
+﻿# https://www.youtube.com/watch?v=QOGKcVsieio
+
+Add-Type -Path "D:\Downloads\itextsharp.dll"
+$filep = "D:\Downloads\sample.pdf"
+
+$pdf = New-Object iTextSharp.text.pdf.PdfReader -ArgumentList $filep
+# $pdf
+# $pdf.NumberOfPages
+$exp = ""
+foreach($page in 1..$($pdf.NumberOfPages)){
+  $text = [iTextSharp.text.pdf.parser.PdfTextExtractor]::GetTextFromPage($pdf, $page)
+  # Write-Output $text
+  $exp += $text
+}
+$pdf.Close()
+
+$exp | Out-File "D:\Downloads\test_file.txt"
+
+# add I_ prefix
+Get-ChildItem $filep | Rename-Item $filep -NewName { "I_" + $_.Name }
+
+
